cff316610411682ca0b07cec739740ba9c50e577
Dmitry Kuznetsov <kuzdmitry1@gmail.com>
2017-10-02 17:11:54 +0400

    Code is now transpiling via Webpack

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const Celio_1 = require("./src/Celio");
+const Celio_1 = require('./src/Celio/index')
 Celio_1.Celio.read('./input/Dystomia.stc').then(console.dir);
 //# sourceMappingURL=index.js.map
